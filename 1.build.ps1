3edfc38877e5d5aa34903739f1b35326b24b5379
Roman Kuzmin <nightroman@gmail.com>
2025-11-29 00:51:14 +0000

    1.build

--- 1.build.ps1
+++ 1.build.ps1
@@ -4,48 +4,48 @@
 #>
 
 Set-StrictMode -Version Latest
-$ModuleName = 'FarLite'
+$_name = 'FarLite'
 
 # Synopsis: Remove temp files.
 task clean {
 	remove z
 }
 
-# Synopsis: Set $Script:Version.
+# Synopsis: Set $Script:_version.
 task version {
-	($Script:Version = Get-BuildVersion Release-Notes.md '##\s+v(\d+\.\d+\.\d+)')
+	($Script:_version = Get-BuildVersion Release-Notes.md '##\s+v(\d+\.\d+\.\d+)')
 }
 
-# Synopsis: Make the package in z\$ModuleName.
+# Synopsis: Make the package in z\$_name.
 task package version, {
 	remove z
-	$null = mkdir z\$ModuleName\Scripts
+	$null = mkdir z\$_name\Scripts
 
-	Copy-Item -Destination z\$ModuleName\Scripts @(
+	Copy-Item -Destination z\$_name\Scripts @(
 		'Scripts\*'
 	)
 
-	Copy-Item -Destination z\$ModuleName @(
-		"about_$ModuleName.help.txt"
-		"$ModuleName.psd1"
-		"$ModuleName.psm1"
+	Copy-Item -Destination z\$_name @(
+		"about_$_name.help.txt"
+		"$_name.psd1"
+		"$_name.psm1"
 		'LICENSE'
 	)
 
 	# set module version
 	Import-Module PsdKit
-	$xml = Import-PsdXml z\$ModuleName\$ModuleName.psd1
-	Set-Psd $xml $Version 'Data/Table/Item[@Key="ModuleVersion"]'
-	Export-PsdXml z\$ModuleName\$ModuleName.psd1 $xml
+	$xml = Import-PsdXml z\$_name\$_name.psd1
+	Set-Psd $xml $_version 'Data/Table/Item[@Key="ModuleVersion"]'
+	Export-PsdXml z\$_name\$_name.psd1 $xml
 }
 
 # Synopsis: Make and push the PSGallery package.
 task pushPSGallery package, {
 	$NuGetApiKey = Read-Host NuGetApiKey
-	Publish-Module -Path z\$ModuleName -NuGetApiKey $NuGetApiKey
+	Publish-Module -Path z\$_name -NuGetApiKey $NuGetApiKey
 },
 clean
 
 task test {
-	Start-Far "ps: Test-FarNet *" $env:FarNetCode\Test\Panels.Lite -Exit 999
+	exec { pwsf "$env:FarNetCode\Test\Panels.Lite" -nop -x 999 -c Test-FarNet.ps1 }
 }
